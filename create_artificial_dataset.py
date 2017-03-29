f0502c13b66086600fa0c9d5f4deb5cce045744b
Javor Kalojanov <javor@cg.uni-saarland.de>
2017-03-29 19:55:55 +0200

    added a semi-automatic script for augmenting the church database using string manipulation

---
+++ create_artificial_dataset.py
@@ -0,0 +1,89 @@
+import argparse
+import os
+import pandas
+import obj_tools
+
+SMILES_COL_NAME = 'structure'
+MAX_WORD_LENGTH = 120
+
+def get_arguments():
+    parser = argparse.ArgumentParser(description='Wavefront .obj to SMILES string conversion')
+    parser.add_argument('in_folder', type=str, help='The folder containing the input .obj files.')
+    parser.add_argument('out_filepath', type=str, help='The output file path in HDF5 format.')
+    parser.add_argument('--smiles_column', type=str, default = SMILES_COL_NAME, help="Name of the column that contains the SMILES strings. Default: %s" % SMILES_COL_NAME)
+    return parser.parse_args()
+
+def str_len_variation_generator(word_list, word, max_length = MAX_WORD_LENGTH, char_pos = 0, depth = 0):
+    if(len(word) > max_length):
+        return
+    if(len(word) <= char_pos or depth >= 2):
+        word_list.append(word)
+        return
+
+    if(word[char_pos] == 'A'):
+        str_len_variation_generator(word_list, word, max_length, char_pos + 1, depth)
+        variant = word[0:char_pos] + 'A' + word[char_pos:]
+        str_len_variation_generator(word_list, variant, max_length, char_pos + 2, depth + 1)
+    else:
+        str_len_variation_generator(word_list, word, max_length, char_pos + 1, depth)
+    return
+
+def str_char_variation_generator(word_list, word, max_length = MAX_WORD_LENGTH, char_pos = 0, depth = 0):
+    if(len(word) <= char_pos or depth >= 3):
+        word_list.append(word)
+        return
+
+    if(word[char_pos] == 'C'):
+        str_char_variation_generator(word_list, word, max_length, char_pos + 1, depth + 1)
+        variant = word[0:char_pos] + 'D' + word[char_pos + 1:] 
+        str_char_variation_generator(word_list, variant, max_length, char_pos + 1, depth + 1)
+    elif(word[char_pos] == 'D'):
+        str_char_variation_generator(word_list, word, max_length, char_pos + 1, depth + 1)
+        variant = word[0:char_pos] + 'C' + word[char_pos + 1:]
+        str_char_variation_generator(word_list, variant, max_length, char_pos + 1, depth + 1)
+    else:
+        str_char_variation_generator(word_list, word, max_length, char_pos + 1, depth)
+    return
+
+def main():
+    args = get_arguments()
+    in_folder = args.in_folder
+
+    #test_strings = []
+    #str_len_variation_generator(test_strings, "A(AAAAB(AD)D)B(AB(AB(C)AB(AAAAB(AD)AC)AAAAAB(AD)AC)C)AAAAAB(D)AD")
+    #final_string = []
+    #for word in test_strings:
+    #    str_char_variation_generator(final_string, word)    
+    #for word in final_string:
+    #    print(word)
+
+    initial_smiles_strings = []
+    for filename in os.listdir(in_folder):
+        if not filename.endswith("_coll_graph.obj") and filename.endswith(".obj"): 
+            current_str = obj_tools.obj2string(in_folder + '/' + filename)
+            print("Converted " + os.path.join(in_folder, filename) + " to " + current_str)
+            if(len(str(current_str)) <= MAX_WORD_LENGTH):
+                initial_smiles_strings.append(str(current_str))           
+            continue
+        else:
+            continue
+    print("# initial strings: " + str(len(initial_smiles_strings)))
+    
+    lengh_variations = []
+    for word in initial_smiles_strings:
+        str_len_variation_generator(lengh_variations, word)
+
+    print("# length variations: " + str(len(lengh_variations)))
+
+    char_variations = []
+    for word in lengh_variations:
+        str_char_variation_generator(char_variations, word)
+
+    print("# char variations: " + str(len(char_variations)))
+
+    df = pandas.DataFrame({args.smiles_column : char_variations})
+    df.to_hdf(args.out_filepath, 'table', format = 'table', data_columns = True)
+
+if __name__ == '__main__':
+
+    main()
--- create_obj_dataset.py
+++ create_obj_dataset.py
@@ -4,6 +4,7 @@ import pandas
 import obj_tools
 
 SMILES_COL_NAME = 'structure'
+MAX_WORD_LENGTH = 120
 
 def get_arguments():
     parser = argparse.ArgumentParser(description='Wavefront .obj to SMILES string conversion')
@@ -16,16 +17,19 @@ def main():
     args = get_arguments()
     in_folder = args.in_folder
 
-    smiles_strings = []
+    initial_smiles_strings = []
     for filename in os.listdir(in_folder):
         if not filename.endswith("_coll_graph.obj") and filename.endswith(".obj"): 
             current_str = obj_tools.obj2string(in_folder + '/' + filename)
             print("Converted " + os.path.join(in_folder, filename) + " to " + current_str)
-            smiles_strings.append(str(current_str))           
+            if(len(str(current_str)) <= MAX_WORD_LENGTH):
+                initial_smiles_strings.append(str(current_str))           
             continue
         else:
             continue
-    df = pandas.DataFrame({args.smiles_column : smiles_strings})
+
+    print("# items: " + str(len(initial_smiles_strings)))
+    df = pandas.DataFrame({args.smiles_column : initial_smiles_strings})
     df.to_hdf(args.out_filepath, 'table', format = 'table', data_columns = True)
 
 if __name__ == '__main__':

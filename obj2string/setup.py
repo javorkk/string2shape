b6aa5323ca2fed9e3690a204a302e87c8b0d96d8
Javor Kalojanov <jkalojanov@cs.rwth-aachen.de>
2017-11-20 17:24:47 +0100

    Fixed obj2string/setup.py and the cmake install process

--- obj2string/setup.py
+++ obj2string/setup.py
@@ -2,6 +2,15 @@ from distutils.core import setup, Extension
 import sys
 
 # the c++ extension module
-extension_mod = Extension("obj_tools", [sys.path[0]+"/obj2string.cpp"])
+obj_tools_mod = Extension(
+	"obj_tools",
+	define_macros = [("MAJOR_VERSION", "1"), ("MINOR_VERSION", "0")],
+	libraries = ["obj2string"],
+	sources = [sys.path[0]+"/obj2string.cpp"])
 
-setup(name = "obj_tools", ext_modules=[extension_mod])
+setup(
+	name = "obj_tools", 
+	version = "1.0",
+	long_description = "Geometry manipulation tools for .obj files.",
+	ext_modules = [obj_tools_mod]
+	)

1a0f88ffbf37087e9129aff8eba248f6d9e4e6ee
Javor Kalojanov <javor@kalojanov.com>
2018-01-12 11:22:46 +0100

    Alternative clustering implementation

--- graph_utils_test.py
+++ graph_utils_test.py
@@ -9,6 +9,9 @@ import neuralnets.grammar as grammar
 from sklearn.cluster import KMeans
 from sklearn.cluster import MeanShift, estimate_bandwidth
 from sklearn.cluster import AffinityPropagation
+from sklearn.cluster import DBSCAN
+
+from sklearn.preprocessing import StandardScaler
 
 import matplotlib.pyplot as plt
 from itertools import cycle
@@ -52,38 +55,83 @@ def process_folder(folder_name, file_list = []):
 # Estimate edge types (categories) via clustering #
 ###################################################
 def categorize_edges(file_list, grammar):
-    all_node_types = np.empty(dtype=int, shape=[0, 2])
-    all_relative_translations = np.empty(dtype=float, shape=[0, 3])
+    all_node_types = np.empty(dtype=int, shape=(0, 2))
+    all_node_ids = np.empty(dtype=int, shape=(0, 2))
+    all_relative_translations = np.empty(dtype=float, shape=(0, 3))
     
     for file_name in file_list:
         graph = ShapeGraph(obj_tools.obj2graph(file_name))
+        if all_node_ids.shape != (0, 2):
+            graph.node_ids = np.add(graph.node_ids, 1 + np.amax(all_node_ids))            
+        all_node_ids = np.append(all_node_ids, graph.node_ids, axis = 0)
         all_node_types = np.append(all_node_types, graph.node_types, axis = 0)
         all_relative_translations = np.append(all_relative_translations, graph.relative_translations, axis = 0)
 
     node_unique_types = (np.unique(all_node_types, axis = 0)).tolist()
-
+    
+    out_cluster_centers = []
     for node_type_pair in node_unique_types:
         ids = np.where((all_node_types == node_type_pair).sum(axis=1) == 2)
+        current_node_ids = all_node_ids[ids]
         current_translations = all_relative_translations[ids]
+
+        if np.prod(ids[0].shape) <= 1:
+            out_cluster_centers.append(current_translations)
+            continue #should not happen
 
         # #############################################################################
         # Cluster using KMeans and max node degree as number of clusters
-        nbr_count = grammar.neighbor_counts[node_type_pair[0] - 1] # -1 because the first element of neigborcounts is empty
-        n_clusters = nbr_count[1]
+        
+        n_clusters = max([pair[1] for pair in grammar.neighbor_counts if pair[0] == grammar.charset[node_type_pair[0]]])
+
         kmeans = KMeans(init='k-means++', n_clusters=n_clusters, n_init=10)
         kmeans.fit(current_translations)
         labels = kmeans.labels_
         cluster_centers = kmeans.cluster_centers_
+
+        unique_ids, counts = np.unique(current_node_ids[:,0], return_counts = True)
+        n_clusters_lower_bound = np.amax(counts)
+        if n_clusters_lower_bound < n_clusters:
+            max_extent = np.max(current_translations, axis = 0)
+            min_extent = np.min(current_translations, axis = 0)
+            bbox_diagonal_length = np.linalg.norm(max_extent - min_extent)
+            
+            cluster_merge_candidates = []
+            for i in range(n_clusters):
+                for j in range(i+1, n_clusters):
+                    dist = np.linalg.norm(cluster_centers[i] - cluster_centers[j])
+                    if  dist < 0.2 * bbox_diagonal_length:
+                        cluster_merge_candidates.append([dist, i, j])
         
+            cluster_merge_candidates.sort(key=lambda tup: tup[0])
+            cluster_merge_candidates = cluster_merge_candidates[0:n_clusters - n_clusters_lower_bound]
+            for candidate_id in range(len(cluster_merge_candidates)):
+                _, i, j = cluster_merge_candidates[candidate_id]
+                labels[labels == j] = i
+                cluster_centers[i] = np.multiply(np.add(cluster_centers[i], cluster_centers[j]), 0.5)
+                np.delete(cluster_centers, j, axis = 0)
+                labels[labels > j] -= 1
+                for k in range(candidate_id + 1, len(cluster_merge_candidates)):
+                    if cluster_merge_candidates[k][1] == j:
+                        cluster_merge_candidates[k][1]  = i
+                    if cluster_merge_candidates[k][1] > j:
+                        cluster_merge_candidates[k][1] -= 1
+                    if cluster_merge_candidates[k][2] == j:
+                        cluster_merge_candidates[k][2]  = i
+                    if cluster_merge_candidates[k][2] > j:
+                        cluster_merge_candidates[k][2] -= 1
+
+            n_clusters = n_clusters - len(cluster_merge_candidates)            
+
         # #############################################################################
         # Compute clustering with MeanShift
 
-        #bandwidth = estimate_bandwidth(current_translations, quantile = 0.15)
+        #bandwidth = estimate_bandwidth(current_translations, quantile = 0.35)
 
         #max_extent = np.max(current_translations, axis = 0)
         #min_extent = np.min(current_translations, axis = 0)
         #bbox_diagonal_length = math.sqrt(np.dot(max_extent - min_extent, max_extent - min_extent))
-        #bandwidth = max(bbox_diagonal_length * 0.25, 0.01)
+        #bandwidth = max(bbox_diagonal_length * 0.35, 0.01)
 
         #ms = MeanShift(bandwidth = bandwidth, bin_seeding = True)
         #ms.fit(current_translations)
@@ -101,10 +149,10 @@ def categorize_edges(file_list, grammar):
         #n_clusters = len(cluster_centers_indices)
         #cluster_centers = current_translations[cluster_centers_indices]
 
-        print("node types:")
-        print(node_type_pair)
-        print("num clusters: " + str(n_clusters))
-    
+        # #############################################################################
+        # Plot Result
+        out_cluster_centers.append(cluster_centers)
+
         plt.figure(1)
         plt.clf()
 
@@ -116,7 +164,47 @@ def categorize_edges(file_list, grammar):
             plt.plot(cluster_center[0], cluster_center[1], 'o', markerfacecolor=col, markeredgecolor='k', markersize=14)
         plt.title('Estimated number of clusters: %d' % n_clusters)
         plt.savefig('plot_' + str(node_type_pair[0]) + '_' + str(node_type_pair[1]) + '.pdf', bbox_inches='tight')
-            
+
+        # #############################################################################
+        # Compute DBSCAN
+        #current_translations = StandardScaler().fit_transform(current_translations)
+
+        #db = DBSCAN(eps=0.3, min_samples=len(file_list)).fit(current_translations)
+        #core_samples_mask = np.zeros_like(db.labels_, dtype=bool)
+        #core_samples_mask[db.core_sample_indices_] = True
+        #labels = db.labels_
+
+        ## Number of clusters in labels, ignoring noise if present.
+        #n_clusters = len(set(labels)) - (1 if -1 in labels else 0)
+        
+        ## Black removed and is used for noise instead.
+        #unique_labels = set(labels)
+        #colors = [plt.cm.Spectral(each)
+        #          for each in np.linspace(0, 1, len(unique_labels))]
+        #for k, col in zip(unique_labels, colors):
+        #    if k == -1:
+        #        # Black used for noise.
+        #        col = [0, 0, 0, 1]
+
+        #    class_member_mask = (labels == k)
+
+        #    xy = current_translations[class_member_mask & core_samples_mask]
+        #    plt.plot(xy[:, 0], xy[:, 1], 'o', markerfacecolor=tuple(col),
+        #             markeredgecolor='k', markersize=14)
+
+        #    xy = current_translations[class_member_mask & ~core_samples_mask]
+        #    plt.plot(xy[:, 0], xy[:, 1], 'o', markerfacecolor=tuple(col),
+        #             markeredgecolor='k', markersize=6)
+
+        #plt.title('Estimated number of clusters: %d' % n_clusters)
+        #plt.show()
+        #plt.savefig('plot_' + str(node_type_pair[0]) + '_' + str(node_type_pair[1]) + '.pdf', bbox_inches='tight')
+
+        print("node types:")
+        print(node_type_pair)
+        print("num clusters: " + str(n_clusters))
+
+    return out_cluster_centers, node_unique_types    
 
 
 def main():
@@ -133,7 +221,10 @@ def main():
     initial_smiles_strings.append(str(obj_tools.obj2string(inputB)))
     tile_grammar = grammar.TilingGrammar(initial_smiles_strings)
 
-    categorize_edges(file_list, tile_grammar)
+    cluster_centers, node_types = categorize_edges(file_list[:100], tile_grammar)
+    
+    #print("cluster centers:")
+    #print(cluster_centers)
 
 
 if __name__ == "__main__":

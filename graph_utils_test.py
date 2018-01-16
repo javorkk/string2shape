Fixed coordinate frame estimation for parts -> third vertex is the most distant to the shape diameter, no averaging if other have similar distance. Part sizes are now passed along other graph information to python better result visualization in graph_test_utils.py

graph_utils_test.py
@@ -13,17 +13,23 @@ from sklearn.cluster import DBSCAN
 
 from sklearn.preprocessing import StandardScaler
 
+from matplotlib import rcParams
+rcParams['font.family'] = 'sans-serif'
+rcParams['font.sans-serif'] = ['Tahoma']
+
 import matplotlib.pyplot as plt
+from mpl_toolkits.mplot3d import Axes3D
 from itertools import cycle
 
 class ShapeGraph():
    
     def __init__(self, input_list):
-        self.node_ids = np.array( [input_list[0::11], input_list[1::11]]).astype(int).T
-        self.node_types = np.array( [input_list[2::11], input_list[3::11]] ).astype(int).T
+        self.node_ids = np.array( [input_list[0::12], input_list[1::12]]).astype(int).T
+        self.node_types = np.array( [input_list[2::12], input_list[3::12]] ).astype(int).T
         self.node_unique_types = (np.unique(self.node_types, axis = 0)).tolist()
-        self.relative_translations = np.array([input_list[4::11], input_list[5::11], input_list[6::11]]).T
-        self.relative_rotations = np.array([input_list[7::11], input_list[8::11], input_list[9::11], input_list[10::11]]).T
+        self.relative_translations = np.array([input_list[4::12], input_list[5::12], input_list[6::12]]).T
+        self.relative_rotations = np.array([input_list[7::12], input_list[8::12], input_list[9::12], input_list[10::12]]).T
+        self.sizes = np.array(input_list[11::12])
 
         #print("node ids:")
         #print(self.node_ids)
@@ -58,6 +64,8 @@ def categorize_edges(file_list, grammar):
     all_node_types = np.empty(dtype=int, shape=(0, 2))
     all_node_ids = np.empty(dtype=int, shape=(0, 2))
     all_relative_translations = np.empty(dtype=float, shape=(0, 3))
+    all_sizes = np.empty(dtype=float,  shape=(1))
+
     
     for file_name in file_list:
         graph = ShapeGraph(obj_tools.obj2graph(file_name))
@@ -66,14 +74,19 @@ def categorize_edges(file_list, grammar):
         all_node_ids = np.append(all_node_ids, graph.node_ids, axis = 0)
         all_node_types = np.append(all_node_types, graph.node_types, axis = 0)
         all_relative_translations = np.append(all_relative_translations, graph.relative_translations, axis = 0)
+        all_sizes = np.append(all_sizes, graph.sizes)
 
     node_unique_types = (np.unique(all_node_types, axis = 0)).tolist()
     
+    fig = plt.figure(figsize=(8.27,11.7 * (1.0 + len(node_unique_types) / 8.0)) ) #A4 page per 8 subplots
+    fig.clf()
+
     out_cluster_centers = []
     for node_type_pair in node_unique_types:
         ids = np.where((all_node_types == node_type_pair).sum(axis=1) == 2)
         current_node_ids = all_node_ids[ids]
         current_translations = all_relative_translations[ids]
+        current_node_size = all_sizes[ids][0]
 
         if np.prod(ids[0].shape) <= 1:
             out_cluster_centers.append(current_translations)
@@ -87,8 +100,38 @@ def categorize_edges(file_list, grammar):
         kmeans = KMeans(init='k-means++', n_clusters=n_clusters, n_init=10)
         kmeans.fit(current_translations)
         labels = kmeans.labels_
-        cluster_centers = kmeans.cluster_centers_
+        cluster_centers = kmeans.cluster_centers_          
 
+        # #############################################################################
+        # Compute clustering with MeanShift
+
+        #bandwidth = estimate_bandwidth(current_translations, quantile = 0.35)
+
+        #max_extent = np.max(current_translations, axis = 0)
+        #min_extent = np.min(current_translations, axis = 0)
+        #bbox_diagonal_length = math.sqrt(np.dot(max_extent - min_extent, max_extent - min_extent))
+        #bandwidth = max(bbox_diagonal_length * 0.35, 0.01)
+        
+        #bandwidth =  0.5 * current_node_size
+
+        #ms = MeanShift(bandwidth = bandwidth, bin_seeding = True)
+        #ms.fit(current_translations)
+        #labels = ms.labels_
+        #cluster_centers = ms.cluster_centers_        
+        #labels_unique = np.unique(labels)
+        #n_clusters = len(labels_unique)
+
+        # #############################################################################
+        # Compute Affinity Propagation
+
+        #af = AffinityPropagation().fit(current_translations)
+        #cluster_centers_indices = af.cluster_centers_indices_
+        #labels = af.labels_
+        #n_clusters = len(cluster_centers_indices)
+        #cluster_centers = current_translations[cluster_centers_indices]
+
+        # #############################################################################
+        # Merge spatially close clusters
         unique_ids, counts = np.unique(current_node_ids[:,0], return_counts = True)
         n_clusters_lower_bound = np.amax(counts)
         if n_clusters_lower_bound < n_clusters:
@@ -100,7 +143,7 @@ def categorize_edges(file_list, grammar):
             for i in range(n_clusters):
                 for j in range(i+1, n_clusters):
                     dist = np.linalg.norm(cluster_centers[i] - cluster_centers[j])
-                    if  dist < 0.2 * bbox_diagonal_length:
+                    if  dist < 0.25 * current_node_size:
                         cluster_merge_candidates.append([dist, i, j])
         
             cluster_merge_candidates.sort(key=lambda tup: tup[0])
@@ -121,49 +164,37 @@ def categorize_edges(file_list, grammar):
                     if cluster_merge_candidates[k][2] > j:
                         cluster_merge_candidates[k][2] -= 1
 
-            n_clusters = n_clusters - len(cluster_merge_candidates)            
-
-        # #############################################################################
-        # Compute clustering with MeanShift
-
-        #bandwidth = estimate_bandwidth(current_translations, quantile = 0.35)
-
-        #max_extent = np.max(current_translations, axis = 0)
-        #min_extent = np.min(current_translations, axis = 0)
-        #bbox_diagonal_length = math.sqrt(np.dot(max_extent - min_extent, max_extent - min_extent))
-        #bandwidth = max(bbox_diagonal_length * 0.35, 0.01)
-
-        #ms = MeanShift(bandwidth = bandwidth, bin_seeding = True)
-        #ms.fit(current_translations)
-        #labels = ms.labels_
-        #cluster_centers = ms.cluster_centers_        
-        #labels_unique = np.unique(labels)
-        #n_clusters = len(labels_unique)
-
-        # #############################################################################
-        # Compute Affinity Propagation
-
-        #af = AffinityPropagation().fit(current_translations)
-        #cluster_centers_indices = af.cluster_centers_indices_
-        #labels = af.labels_
-        #n_clusters = len(cluster_centers_indices)
-        #cluster_centers = current_translations[cluster_centers_indices]
+            n_clusters = n_clusters - len(cluster_merge_candidates)  
 
         # #############################################################################
         # Plot Result
         out_cluster_centers.append(cluster_centers)
 
-        plt.figure(1)
-        plt.clf()
-
         colors = cycle('bgrcmykbgrcmykbgrcmykbgrcmyk')
+
+        subplot_id = 1 + node_unique_types.index(node_type_pair)
+        ax = fig.add_subplot((1 + len(node_unique_types)) / 2, 2, subplot_id, projection='3d')
+        ax.set_xlabel('x')
+        ax.set_ylabel('y')
+        ax.set_zlabel('z')
+        xyz_max = np.amax(current_translations)
+        xyz_min = np.amin(current_translations)
+        ax.set_xlim([xyz_min,xyz_max])
+        ax.set_ylim([xyz_min,xyz_max])
+        ax.set_zlim([xyz_min,xyz_max])
+
+        ax.set_title('edge type [' + grammar.charset[node_type_pair[0]] + ', ' + grammar.charset[node_type_pair[1]] + '] clusters: %d' % n_clusters)
         for k, col in zip(range(n_clusters), colors):
             my_members = labels == k
             cluster_center = cluster_centers[k]
-            plt.plot(current_translations[my_members, 0], current_translations[my_members, 1], col + '.')
-            plt.plot(cluster_center[0], cluster_center[1], 'o', markerfacecolor=col, markeredgecolor='k', markersize=14)
-        plt.title('Estimated number of clusters: %d' % n_clusters)
-        plt.savefig('plot_' + grammar.charset[node_type_pair[0]] + '_' + grammar.charset[node_type_pair[1]] + '.pdf', bbox_inches='tight')
+            ax.scatter(current_translations[my_members, 0], current_translations[my_members, 1], current_translations[my_members, 2], c = col, marker = '.')
+            ax.scatter(cluster_center[0], cluster_center[1], cluster_center[2], c = col, marker = 'o')
+
+        #for k, col in zip(range(n_clusters), colors):
+        #    my_members = labels == k
+        #    cluster_center = cluster_centers[k]
+        #    plt.plot(current_translations[my_members, 0], current_translations[my_members, 2], col + '.')
+        #    plt.plot(cluster_center[0], cluster_center[2], 'o', markerfacecolor=col, markeredgecolor='k', markersize=14)
 
         # #############################################################################
         # Compute DBSCAN
@@ -202,6 +233,8 @@ def categorize_edges(file_list, grammar):
 
 
         print("node types: [" + grammar.charset[node_type_pair[0]] + ", " + grammar.charset[node_type_pair[1]] + "] num clusters : " + str(n_clusters))
+        
+    plt.savefig('edge_configuration_plot.pdf', bbox_inches='tight')
 
     return out_cluster_centers, node_unique_types    
 
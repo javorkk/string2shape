3489fdb63e31bd801ac6b00ed67f643b37b94632
Javor Kalojanov <jkalojanov@cs.rwth-aachen.de>
2018-09-26 11:21:03 +0200

    added triplet contrastive loss for the VAE

--- neuralnets/autoencoder.py
+++ neuralnets/autoencoder.py
@@ -106,9 +106,10 @@ class Tiling_LSTM_VAE():
                weights_file = None):
         charset_length = len(charset)
         
-        x = Input(shape=(max_length, charset_length))
-        _, z = self._buildEncoder(x, latent_rep_size, max_length)
-        self.encoder = Model(x, z)
+        x = Input(shape=(max_length, charset_length), name='main_input')
+
+        _, latent_x = self._buildEncoder(x, latent_rep_size, max_length)
+        self.encoder = Model(x, latent_x)
 
         encoded_input = Input(shape=(latent_rep_size,))
         self.decoder = Model(
@@ -121,12 +122,12 @@ class Tiling_LSTM_VAE():
             )
         )
 
-        x1 = Input(shape=(max_length, charset_length))
-        vae_loss, z1 = self._buildEncoder(x1, latent_rep_size, max_length)
+        ae_input = Input(shape=(max_length, charset_length), name='main_input')
+        vae_loss,  ae_latent_z = self._buildEncoder(ae_input, latent_rep_size, max_length)
         self.autoencoder = Model(
-            x1,
+            ae_input,
             self._buildDecoder(
-                z1,
+                ae_latent_z,
                 latent_rep_size,
                 max_length,
                 charset_length
@@ -138,7 +139,8 @@ class Tiling_LSTM_VAE():
             self.encoder.load_weights(weights_file, by_name = True)
             self.decoder.load_weights(weights_file, by_name = True)
 
-        self.autoencoder.compile(optimizer = 'Adam',
+        opt = optimizers.Adam(lr=0.00001, amsgrad=True)
+        self.autoencoder.compile(optimizer = opt,
                                  loss = vae_loss,
                                  metrics = ['accuracy'])
 
@@ -179,6 +181,143 @@ class Tiling_LSTM_VAE():
     def load(self, charset, weights_file, max_w_length=120, latent_rep_size=292):
         self.create(charset, weights_file=weights_file,  max_length=max_w_length, latent_rep_size=latent_rep_size)
 
+class Tiling_Triplet_LSTM_VAE():
+
+    autoencoder = None
+    
+    def create(self,
+               charset,
+               max_length = 120,
+               latent_rep_size = 292,
+               weights_file = None):
+        charset_length = len(charset)
+        
+        x = Input(shape=(max_length, charset_length), name='main_input')
+        y = Input(shape=(max_length, charset_length), name='positive_input')
+        z = Input(shape=(max_length, charset_length), name='negative_input')
+
+        _, latent_x = self._buildEncoder((x,y,z), latent_rep_size, max_length)
+        self.encoder = Model(x, latent_x)
+
+        encoded_input = Input(shape=(latent_rep_size,))
+        self.decoder = Model(
+            encoded_input,
+            self._buildDecoder(
+                encoded_input,
+                latent_rep_size,
+                max_length,
+                charset_length
+            )
+        )
+
+        # y = Input(shape=(max_length, charset_length), name='positive_input')
+        # _, latent_y = self._buildEncoder(y, latent_rep_size, max_length)
+        # z = Input(shape=(max_length, charset_length), name='negative_input')
+        # _, latent_z = self._buildEncoder(z, latent_rep_size, max_length)
+
+        # ae_input = Input(shape=(max_length, charset_length), name='main_input')
+        vae_loss,  latent_x = self._buildEncoder((x,y,z), latent_rep_size, max_length)
+        self.autoencoder = Model(
+            (x,y,z),
+            self._buildDecoder(
+                latent_x,
+                latent_rep_size,
+                max_length,
+                charset_length
+            )
+        )
+
+        # contrastive loss on sampled latent point coordinates
+        # def triplet_loss(x_in, x_pred):
+        #     lat_x = K.flatten(latent_x)
+        #     pos_y = K.flatten(latent_y)
+        #     neg_z = K.flatten(latent_z)
+        #     d_pos = lat_x - pos_y
+        #     d_neg = lat_x - neg_z
+        #     tr_loss = K.max(0.0, 1.0 + K.dot(d_pos, d_pos, axis=-1) - K.dot(d_neg, d_neg, axis=-1))
+        #     return tr_loss
+
+        if weights_file:
+            self.autoencoder.load_weights(weights_file)
+            self.encoder.load_weights(weights_file, by_name = True)
+            self.decoder.load_weights(weights_file, by_name = True)
+
+        opt = optimizers.Adam(lr=0.0001, amsgrad=True)
+        self.autoencoder.compile(optimizer = opt,
+                                 loss = vae_loss,
+                                 metrics = ['accuracy'])
+
+    def _buildEncoder(self, input_triple, latent_rep_size, max_length, epsilon_std = 0.01):
+        x,y,z = input_triple
+        h = LSTM(301, return_sequences = True, name='in_lstm_1')(x)
+        h = LSTM(301, return_sequences = True, name='in_lstm_2')(h)
+        h = Flatten(name='flatten_1')(h)
+        h = Dense(435, activation = 'relu', name='dense_1')(h)
+
+        z_mean_x = Dense(latent_rep_size, name='z_mean', activation = 'linear')(h)
+        z_log_var_x = Dense(latent_rep_size, name='z_log_var', activation = 'linear')(h)
+
+        latent_x = Lambda(sampling, output_shape=(latent_rep_size,), name='lambda')([z_mean_x, z_log_var_x])
+
+        h_y = LSTM(301, return_sequences = True, name='in_lstm_1')(y)
+        h_y = LSTM(301, return_sequences = True, name='in_lstm_2')(h_y)
+        h_y = Flatten(name='flatten_1')(h_y)
+        h_y = Dense(435, activation = 'relu', name='dense_1')(h_y)
+
+        z_mean_y = Dense(latent_rep_size, name='z_mean', activation = 'linear')(h_y)
+        z_log_var_y = Dense(latent_rep_size, name='z_log_var', activation = 'linear')(h_y)
+
+        latent_y = Lambda(sampling, output_shape=(latent_rep_size,), name='lambda')([z_mean_y, z_log_var_y])
+
+        h_z = LSTM(301, return_sequences = True, name='in_lstm_1')(z)
+        h_z = LSTM(301, return_sequences = True, name='in_lstm_2')(h_z)
+        h_z = Flatten(name='flatten_1')(h_z)
+        h_z = Dense(435, activation = 'relu', name='dense_1')(h_z)
+
+        z_mean_z = Dense(latent_rep_size, name='z_mean', activation = 'linear')(h_z)
+        z_log_var_z = Dense(latent_rep_size, name='z_log_var', activation = 'linear')(h_z)
+
+        latent_z = Lambda(sampling, output_shape=(latent_rep_size,), name='lambda')([z_mean_z, z_log_var_z])
+
+
+        def sampling(args):
+            z_mean_, z_log_var_ = args
+            batch_size = K.shape(z_mean_)[0]
+            epsilon = K.random_normal(shape=(batch_size, latent_rep_size), mean=0., stddev = epsilon_std)
+            return z_mean_ + K.exp(z_log_var_ / 2) * epsilon
+
+
+        def vae_loss(x_in, x_decoded_mean):
+            x,y,z = x_in
+            x = K.flatten(x)
+            x_decoded_mean = K.flatten(x_decoded_mean)
+            #cross entropy
+            xent_loss = max_length * objectives.binary_crossentropy(x, x_decoded_mean)
+            #Kullback-Leibler regularization
+            kl_loss = - 0.125 * K.sum(1 + z_log_var_x - K.square(z_mean_x) - K.exp(z_log_var_x), axis = -1)
+            #triplet contrastive loss on Euclidean distance in latent space
+            flat_x = K.flatten(latent_x)
+            flat_y = K.flatten(latent_y)
+            flat_z = K.flatten(latent_z)
+            d_pos = flat_x - flat_y
+            d_neg = flat_x - flat_z
+            tr_loss = K.max(0.0, 1.0 + K.dot(d_pos, d_pos, axis=-1) - K.dot(d_neg, d_neg, axis=-1))
+            return xent_loss + kl_loss + tr_loss
+
+        return (vae_loss, latent_x)
+
+    def _buildDecoder(self, z, latent_rep_size, max_length, charset_length):
+        h = Dense(latent_rep_size, name='latent_input', activation = 'relu')(z)
+        h = RepeatVector(max_length, name='repeat_vector')(h)
+        h = LSTM(501, return_sequences=True, name='out_lstm_1')(h)
+        h = LSTM(501, return_sequences=True, name='out_lstm_2')(h)
+        return TimeDistributed(Dense(charset_length, activation='softmax'), name='decoded_mean')(h)
+
+    def save(self, filename):
+        self.autoencoder.save_weights(filename)
+    
+    def load(self, charset, weights_file, max_w_length=120, latent_rep_size=292):
+        self.create(charset, weights_file=weights_file,  max_length=max_w_length, latent_rep_size=latent_rep_size)
 
 class Tiling_LSTM_VAE_XL():
 
@@ -224,7 +363,7 @@ class Tiling_LSTM_VAE_XL():
             self.decoder.load_weights(weights_file, by_name = True)
 
         #optim = optimizers.Adam(lr=0.0001, amsgrad=True)
-        optim = optimizers.Adam(lr=0.0005, amsgrad=True)
+        optim = optimizers.Adam(lr=0.00001, amsgrad=True)
         self.autoencoder.compile(optimizer = optim,
                                  loss = vae_loss,
                                  metrics = ['accuracy'])
--- train.py
+++ train.py
@@ -4,11 +4,14 @@ import argparse
 import os
 import h5py
 import numpy as np
+import random
 
-from neuralnets.autoencoder import TilingVAE, Tiling_LSTM_VAE, Tiling_LSTM_VAE_XL
+import neuralnets.grammar as grammar
 from neuralnets.utils import one_hot_array, one_hot_index, from_one_hot_array, \
     decode_smiles_from_indexes, load_dataset
-from keras.callbacks import ModelCheckpoint, ReduceLROnPlateau, Callback
+
+from neuralnets.autoencoder import TilingVAE, Tiling_LSTM_VAE, Tiling_Triplet_LSTM_VAE, Tiling_LSTM_VAE_XL
+from keras.callbacks import ModelCheckpoint, ReduceLROnPlateau, Callback, CSVLogger
 
 from keras.utils import plot_model
 import matplotlib.pyplot as plt
@@ -19,13 +22,14 @@ rcParams['font.sans-serif'] = ['Verdana']
 NUM_EPOCHS = 1
 BATCH_SIZE = 200
 LATENT_DIM = 292
-TYPE = 'simple'
+TYPE = 'lstm'
 
 def get_arguments():
     parser = argparse.ArgumentParser(description='Molecular autoencoder network')
     parser.add_argument('data', type=str, help='The HDF5 file containing preprocessed data.')
     parser.add_argument('model', type=str,
                         help='Where to save the trained model. If this file exists, it will be opened and resumed.')
+    parser.add_argument('--grammar', type=str, default='', help='Tiling grammar. Activates (and necessary for) triplet learning')
     parser.add_argument('--epochs', type=int, metavar='N', default=NUM_EPOCHS,
                         help='Number of epochs to run during training.')
     parser.add_argument('--latent_dim', type=int, metavar='N', default=LATENT_DIM,
@@ -33,7 +37,7 @@ def get_arguments():
     parser.add_argument('--batch_size', type=int, metavar='N', default=BATCH_SIZE,
                         help='Number of samples to process per minibatch during training.')
     parser.add_argument('--type', type=str, default=TYPE,
-                        help='What type model to train: simple, lstm, lstm_, gru.')
+                        help='What type model to train: simple, lstm, lstm_large.')
     return parser.parse_args()
 
 class PlotLearning(Callback):
@@ -64,7 +68,7 @@ class PlotLearning(Callback):
         f, (ax1, ax2) = plt.subplots(1, 2, sharex=True)
 
         ax1.plot(self.x, self.losses, label="loss")
-        ax1.plot(self.x, self.val_losses, label="val_loss")
+        ax1.plot(self.x, self.val_losses, label="validation loss")
         ax1.legend()
         
         ax2.plot(self.x, self.acc, label="accuracy")
@@ -94,14 +98,17 @@ def main():
     #    print(exaple)
 
     #return
+
     if args.type == 'lstm':
         model = Tiling_LSTM_VAE()
-    elif args.type == 'gru':
-        model = Tiling_GRU_VAE()
-    elif args.type == 'lstm_':
+    elif args.type == 'lstm_large':
         model = Tiling_LSTM_VAE_XL()
-    else:
+    elif args.type == 'simple':
         model = TilingVAE()
+    elif os.path.isfile(args.grammar):
+        model = Tiling_Triplet_LSTM_VAE()
+    else:
+        model = Tiling_LSTM_VAE()
 
     if os.path.isfile(args.model):
         model.load(charset, args.model, max_w_length=word_length, latent_rep_size=args.latent_dim)
@@ -109,37 +116,85 @@ def main():
         model.create(charset, max_length=word_length, latent_rep_size=args.latent_dim)
 
     checkpointer = ModelCheckpoint(filepath = args.model,
-                                   verbose = 1,
-                                   save_best_only = True)
+                                verbose = 1,
+                                save_best_only = True)
 
     reduce_lr = ReduceLROnPlateau(monitor = 'val_loss',
-                                  factor = 0.2,
-                                  patience = 3,
-                                  min_lr = 0.00001)
+                                factor = 0.2,
+                                patience = 3,
+                                min_lr = 0.000001)
 
     filename, ext = os.path.splitext(args.model) 
     plot_model(model.autoencoder, to_file=filename + '_nn.pdf', show_shapes=True)
 
+    csv_logger = CSVLogger(filename + '_training.log')
 
     plot = PlotLearning()
     plot.set_filename(filename)
 
-    history = model.autoencoder.fit(
-        data_train,
-        data_train,
-        shuffle = True,
-        epochs = args.epochs,
-        batch_size = args.batch_size,
-        callbacks = [checkpointer, reduce_lr, plot],
-        validation_data = (data_test, data_test)
-    )
+    if os.path.isfile(args.grammar):
+        tiling_grammar = grammar.TilingGrammar([])
+        tiling_grammar.load(args.grammar)
+        data_y = data_train
+        data_z = data_train
 
-#     # summarize history for loss
-#     plt.plot(history.history['val_loss'])
-#     plt.title('model loss')
-#     plt.ylabel('loss')
-#     plt.xlabel('epoch')
-#     plt.savefig(filename + '_loss_history.pdf', bbox_inches='tight')
+        reduce_lr = ReduceLROnPlateau(monitor = 'val_loss',
+                                    factor = 0.2,
+                                    patience = 3,
+                                    min_lr = 0.000001)
+
+        for item_id in range(len(data_train)):
+            train_str = decode_smiles_from_indexes(map(from_one_hot_array, data_train[item_id]), charset)
+
+            sample_ids = np.random.randint(len(data_train), size=100)
+            str_samples = [decode_smiles_from_indexes(map(from_one_hot_array, data_train[x]), charset) for x in sample_ids]
+            str_distances = [tiling_grammar.word_similarity(train_str, x) for x in str_samples]
+            idx_y = str_distances.index(min(str_distances))
+            idx_z = str_distances.index(max(str_distances))
+            data_y[item_id] = data_train[sample_ids[idx_y]]
+            data_z[item_id] = data_train[sample_ids[idx_z]]
+
+        test_y = data_test
+        test_z = data_test
+        for item_id in range(len(data_test)):
+            test_str = decode_smiles_from_indexes(map(from_one_hot_array, data_test[item_id]), charset)
+
+            sample_ids = np.random.randint(len(data_test), size=100)
+            str_samples = [decode_smiles_from_indexes(map(from_one_hot_array, data_test[x]), charset) for x in sample_ids]
+            str_distances = [tiling_grammar.word_similarity(test_str, x) for x in str_samples]
+            idx_y = str_distances.index(min(str_distances))
+            idx_z = str_distances.index(max(str_distances))
+            test_y[item_id] = data_test[sample_ids[idx_y]]
+            test_z[item_id] = data_test[sample_ids[idx_z]]
+
+        history = model.autoencoder.fit(
+            {'main_input':data_train, 'positive_input':data_y, 'negative_input':data_z},
+            data_train,
+            shuffle = True,
+            epochs = args.epochs,
+            batch_size = args.batch_size,
+            callbacks = [checkpointer, reduce_lr, plot, csv_logger],
+            validation_data = ({'main_input':data_test, 'positive_input':test_y, 'negative_input':test_z}, data_test)
+        )
+
+    else:
+
+        history = model.autoencoder.fit(
+            data_train,
+            data_train,
+            shuffle = True,
+            epochs = args.epochs,
+            batch_size = args.batch_size,
+            callbacks = [checkpointer, reduce_lr, plot, csv_logger],
+            validation_data = (data_test, data_test)
+        )
+
+    # summarize history for loss
+    # plt.plot(history.history['val_loss'])
+    # plt.title('loss breakdown')
+    # plt.ylabel('loss')
+    # plt.xlabel('epoch')
+    # plt.savefig(filename + '_losses_breakdown.pdf', bbox_inches='tight')
 
 if __name__ == '__main__':
     main()
